f03cf41a97257848805d847f1a43358aacdfa188
Ryoichi SHINBORI <pragruic@gmail.com>
2020-02-26 14:39:51 +0900

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+
+const IndexPage: NextPage = () => (
+  <div className="m-4">
+    <div className="p-2 rounded shadow bg-gray-200">
+      <span className="m-1 text-gray-700 text-xl">NEXT.js</span>
+      <span className="m-1 text-gray-500">with Tailwind CSS</span>
+    </div>
+  </div>
+)
+
+export default IndexPage
